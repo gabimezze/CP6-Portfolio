55b220bcdba9f148a49b41541acd530385c35354
gabimezze <gabrielagomesceza@gmail.com>
2024-10-24 21:46:00 -0300

    renomeando types

---
+++ src/app/types/nota.ts
@@ -0,0 +1,14 @@
+
+// types/Nota.ts
+export interface Nota {
+  id: number;
+  tipo: 'CP' | 'CS' | 'GS'; // CP = Checkpoint, CS = Challenge Sprint, GS = Global Solution
+  semestre: 1 | 2;
+  valor: number;
+}
+
+export interface Boletim {
+  cp: Nota[];
+  cs: Nota[];
+  gs: Nota[];
+}
