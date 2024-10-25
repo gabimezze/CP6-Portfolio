f58fbe51e6b2b741f281ebcaf068322d8c1a9015
gabimezze <gabrielagomesceza@gmail.com>
2024-10-24 21:46:20 -0300

    estilizando com tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <div className="text-center text-4xl mt-32 mb-0 mt-0 text-white bg-gradient-to-r from-my-gradient-100 to-my-gradient-50 p-52">
+    <div className="text-center text-4xl mb-0 mt-0 text-white bg-gradient-to-r from-my-gradient-100 to-my-gradient-50 p-52">
       <h1 className="font-medium">Olá!</h1>
-      <h2>Seja bem-vindo.</h2>
+      <h2>Seja Bem-vindo.</h2>
     </div>
   );
 }
