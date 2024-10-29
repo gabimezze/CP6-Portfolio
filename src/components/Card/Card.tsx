e01b8e20ee04d32fffcda50928f1669787c767ce
anacarolcg0 <anacarolcg0@gmail.com>
2024-10-29 11:06:17 -0300

    arrumando card com imagens

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,21 @@
 // components/Card/Card.tsx
 import React from 'react';
+import { StaticImageData } from 'next/image';
 
 interface CardProps {
   name: string;
   rm: string;
-  imageUrl: string;
+  imageUrl: string | StaticImageData;
 }
 
 const Card = ({ name, rm, imageUrl}: CardProps) => {
+  
+  const imageSrc = typeof imageUrl === 'string' ? imageUrl : imageUrl.src;
+
   return (
     <div className="w-[200px] h-[100px] bg-[#f308b0] border border-gray-#000000 rounded-lg p-5 shadow-md m-5">
       <div className="flex justify-center mb-5">
-        <img src={imageUrl} alt={name} className="w-[130px] h-[130px] rounded-full" />
+      <img src={imageSrc} alt={name} className="w-[130px] h-[130px] rounded-full" />
       </div>
       <h2 className="text-lg font-bold text-black mb-2">{name}</h2>
       <p className="text-base text-black mb-5">{rm}</p>
