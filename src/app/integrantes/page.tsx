4da3c5b925223fa81dad7bed27c8de30e6ca3c1d
mateuscatronappe <teteucastro31@gmail.com>
2024-10-29 11:40:55 -0300

    envolvendo link nos cards para poder acessar o boletim

--- src/app/integrantes/page.tsx
+++ src/app/integrantes/page.tsx
@@ -1,37 +1,41 @@
-// integrantes/page.tsx
 import Card from '../../components/Card/Card';
-import intAna from '@/img/integrante-ana.png'
-import intGabi from '@/img/integrante-gabriela.png'
-import intLu from '@/img/integrante-luisa.png'
-import intMat from '@/img/integrante-mateus.png'
-import intMi from '@/img/integrante-michelle.png'
+import intAna from '@/img/integrante-ana.png';
+import intGabi from '@/img/integrante-gabriela.png';
+import intLu from '@/img/integrante-luisa.png';
+import intMat from '@/img/integrante-mateus.png';
+import intMi from '@/img/integrante-michelle.png';
 
 const Integrantes = () => {
   const integrantesData = [
     { 
       name: 'Ana Carolina de Castro Gonçalves', 
-      rm:'RM: 554669', 
-      imageUrl:intAna, 
+      rm: 'RM: 554669', 
+      imageUrl: intAna, 
+      link: '/pages/ana' 
     },
     { 
-      name:'Gabriela Gomes Cezar', 
-      rm:'RM: 556941', 
-      imageUrl:intGabi, 
+      name: 'Gabriela Gomes Cezar', 
+      rm: 'RM: 556941', 
+      imageUrl: intGabi, 
+      link: '/pages/gabi' 
     },
     { 
       name: 'Luisa Danielle', 
       rm: 'RM: 555292', 
-      imageUrl:intLu, 
+      imageUrl: intLu, 
+      link: '/pages/lu' 
     },
     { 
-      name:'Mateus de Castro Nappe', 
-      rm:'RM: 556474', 
-      imageUrl:intMat, 
+      name: 'Mateus de Castro Nappe', 
+      rm: 'RM: 556474', 
+      imageUrl: intMat, 
+      link: '/pages/mat'
     },
     { 
-      name:'Michelle Potenza', 
-      rm:'RM: 557702', 
-      imageUrl:intMi, 
+      name: 'Michelle Potenza', 
+      rm: 'RM: 557702', 
+      imageUrl: intMi, 
+      link: '/pages/mi'
     },
   ];
 
@@ -44,6 +48,7 @@ const Integrantes = () => {
             name={integrante.name} 
             rm={integrante.rm} 
             imageUrl={integrante.imageUrl} 
+            link={integrante.link} 
           />
         ))}
       </div>
