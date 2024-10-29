0cf73f178a3732275198918cc55fb3b4caec069d
gabimezze <gabrielagomesceza@gmail.com>
2024-10-29 18:01:34 -0300

    estiliando home, cabecalho e rodape

--- cp3/src/app/page.tsx
+++ cp3/src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="text-center text-4xl mb-0 mt-0 h-screen text-white bg-gradient-to-r from-my-gradient-100 to-my-gradient-50 p-52">
+    <div className="text-center text-4xl mb-0 mt-0 text-white bg-gradient-to-r from-my-gradient-100 to-my-gradient-50 p-52">
       <h1 className="font-medium">Olá!</h1>
       <h2>Seja Bem-vindo.</h2>
     </div>
--- cp3/src/components/Rodape/Rodape.tsx
+++ cp3/src/components/Rodape/Rodape.tsx
@@ -1,6 +1,6 @@
 export default function Rodape() {
     return (
-        <footer className="bg-gradient-to-r from-my-gradient-100 to-my-gradient-50 mb-0 h-screen mt-0 text-center p-20 text-white">
+        <footer className="bg-gradient-to-r from-my-gradient-100 to-my-gradient-50 mb-0 h-96 mt-0 text-center p-20 text-white">
             <p>&copy; Desenvolvido por <span>Ana Gonçalves, Gabriela Gomes, Luisa Danielle, Mateus Nappe, Michelle Potenza</span></p>
         </footer>
     )
