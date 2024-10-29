9f28a2f0983bb370f3ad8b8ace80bf7742ae555f
mateuscatronappe <teteucastro31@gmail.com>
2024-10-29 10:26:36 -0300

    ajustando componente card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,27 +1,21 @@
-// components/Card.tsx
+// components/Card/Card.tsx
 import React from 'react';
 
 interface CardProps {
   name: string;
-  email: string;
+  rm: string;
   imageUrl: string;
-  socialLinks: { url: string; iconUrl: string }[];
 }
 
-const Card = ({ name, email, imageUrl, socialLinks }: CardProps) => {
+const Card = ({ name, rm, imageUrl}: CardProps) => {
   return (
-    <div className="w-[300px] h-[400px] bg-[#06426f] border border-gray-300 rounded-lg p-5 shadow-md m-5">
+    <div className="w-[200px] h-[100px] bg-[#f308b0] border border-gray-#000000 rounded-lg p-5 shadow-md m-5">
       <div className="flex justify-center mb-5">
         <img src={imageUrl} alt={name} className="w-[130px] h-[130px] rounded-full" />
       </div>
-      <h2 className="text-lg font-bold text-white mb-2">{name}</h2>
-      <p className="text-base text-white mb-5">{email}</p>
+      <h2 className="text-lg font-bold text-black mb-2">{name}</h2>
+      <p className="text-base text-black mb-5">{rm}</p>
       <div className="flex justify-center mt-24">
-        {socialLinks.map((link, index) => (
-          <a key={index} href={link.url} className="mx-2">
-            <img src={link.iconUrl} alt="Social Icon" className="w-[30px] h-[30px]" />
-          </a>
-        ))}
       </div>
     </div>
   );
