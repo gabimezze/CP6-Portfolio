f1da9516348b3f54f29da7c8d461a483b2c3f846
mateuscatronappe <teteucastro31@gmail.com>
2024-10-29 11:42:08 -0300

    adicionando rota da mi

---
+++ src/app/pages/mi/page.tsx
@@ -0,0 +1,10 @@
+import Table from '../../../components/Table/Table';
+
+export default function Boletim() {
+  return (
+    <div>
+      <h1 className=" text-3xl mt-16 font-mediu text-center mb-6">Boletim Acadêmico de Michelle</h1>
+      <Table />
+    </div>
+  );
+}
