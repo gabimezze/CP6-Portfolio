6bbb4f2552c04536e2c38eec7879e49d9165f853
mateuscatronappe <teteucastro31@gmail.com>
2024-10-29 11:41:44 -0300

    adicionando rota da gabi

---
+++ src/app/pages/gabi/page.tsx
@@ -0,0 +1,11 @@
+
+import Table from '../../../components/Table/Table';
+
+export default function Boletim() {
+  return (
+    <div>
+      <h1 className=" text-3xl mt-16 font-mediu text-center mb-6">Boletim Acadêmico de Gabriela</h1>
+      <Table />
+    </div>
+  );
+}
