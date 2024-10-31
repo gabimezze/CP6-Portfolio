fbd079a24c98c3dffd43825291617f5190675a43
mateuscastronappe <teteucastro31@gmail.com>
2024-10-30 22:52:51 -0300

    criando uma const com o nome do integrante para facilitar o codigo

--- cp3/src/app/pages/gabi/page.tsx
+++ cp3/src/app/pages/gabi/page.tsx
@@ -1,11 +1,12 @@
-
 import Table from '../../../components/Table/Table';
 
 export default function Boletim() {
+  const integrante = "Gabriela"; 
+
   return (
     <div>
-      <h1 className=" text-3xl mt-16 font-mediu text-center mb-6">Boletim Acadêmico de Gabriela</h1>
-      <Table />
+      <h1 className="text-3xl mt-16 font-medium text-center mb-6">Boletim Acadêmico de {integrante}</h1>
+      <Table integrante={integrante}/> 
     </div>
   );
 }
